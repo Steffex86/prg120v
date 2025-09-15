8db7e1b251d218db07cd4c9568e61331c1fd3771
Steffex86 <steff_valseth@hotmail.com>
2025-09-15 20:52:22 +0200

    Test 2

---
+++ Test 2.php
@@ -0,0 +1,17 @@
+<?php 
+$tall1=4;
+$tall2=5;
+
+if ($tall1==$tall2)
+  {
+    print("Tallene er like");
+  }
+else if ($tall1!=$tall2)
+  {
+    print("Tallene er ikke like");
+  }
+else
+  {
+    print("Ikke mulig å avgjøre om tallene er like");
+  }
+?>
