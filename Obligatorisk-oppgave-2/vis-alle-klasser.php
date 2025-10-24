c0c9b83d6ac17ca0e5e7c8cee53eaf038d9c60ed
Steffex86 <steff_valseth@hotmail.com>
2025-10-24 20:42:29 +0200

    visalleklasser

--- Obligatorisk-oppgave-2/vis-alle-klasser.php
+++ Obligatorisk-oppgave-2/vis-alle-klasser.php
@@ -2,26 +2,26 @@
 /*
 /*  Programmet skriver ut alle registrerte klasser
 */
-  include("db-tilkobling.php");  /* tilkobling til database-serveren utf�rt og valg av database foretatt */
+  include("Obligatorisk-oppgave-2\db-tilkobling.php");  /* tilkobling til database-serveren utført og valg av database foretatt */
 
-  $sqlSetning="SELECT * FROM poststed;";
+  $sqlSetning="SELECT * FROM klasse;";
   
   $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
     /* SQL-setning sendt til database-serveren */
 	
   $antallRader=mysqli_num_rows($sqlResultat);  /* antall rader i resultatet beregnet */
 
-  print ("<h3>Registrerte poststeder</h3>");
+  print ("<h3>Registrerte klasser</h3>");
   print ("<table border=1>");  
-  print ("<tr><th align=left>postnr</th> <th align=left>poststed</th></tr>"); 
+  print ("<tr><th align=left>klassekode</th> <th align=left>klassenavn</th></tr>"); 
 
   for ($r=1;$r<=$antallRader;$r++)
     {
-      $rad=mysqli_fetch_array($sqlResultat);  /* ny rad hentet fra sp�rringsresultatet */
-      $postnr=$rad["postnr"];        /* ELLER $postnr=$rad[0]; */
-      $poststed=$rad["poststed"];    /* ELLER $poststed=$rad[1]; */
+      $rad=mysqli_fetch_array($sqlResultat);  /* ny rad hentet fra spørringsresultatet */
+      $klassekode=$rad["klassekode"];        /* ELLER $postnr=$rad[0]; */
+      $klasssenavn=$rad["klasssenavn"];    /* ELLER $poststed=$rad[1]; */
 
-      print ("<tr> <td> $postnr </td> <td> $poststed </td> </tr>");
+      print ("<tr> <td> $klassekode </td> <td> $klasssenavn </td> </tr>");
     }
   print ("</table>"); 
 ?>
