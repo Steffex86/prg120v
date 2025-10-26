a6e1d7689966279fc8400e57a82533a2a10d4112
Steffex86 <steff_valseth@hotmail.com>
2025-10-26 08:23:20 +0100

    semikolon

--- Obligatorisk-oppgave-2/slett-klasse.php
+++ Obligatorisk-oppgave-2/slett-klasse.php
@@ -36,7 +36,7 @@
           mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; slette data i databasen");
             /* SQL-setning sendt til database-serveren */
 		
-          print ("F&oslash;lgende klasse er n&aring; slettet: $klassekode $klassenavn $studiumkode $<br />");
+          print ("F&oslash;lgende klasse er n&aring; slettet: $klassekode; $klassenavn; $studiumkode; $<br />");
         }	
     }
 ?>
